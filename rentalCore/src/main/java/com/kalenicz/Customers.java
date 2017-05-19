302907131a52774d0df35e4c3fb4566568a9f0e3
mkalenicz <maciej.kalenicz@gmail.com>
2017-05-19 20:52:21 +0200

    add customers

--- rentalCore/src/main/java/com/kalenicz/Customers.java
+++ rentalCore/src/main/java/com/kalenicz/Customers.java
@@ -4,4 +4,49 @@ package com.kalenicz;
  * Created by RENT on 2017-05-19.
  */
 public class Customers {
+    private String pesel;
+    private String name;
+    private String surname;
+    private String city;
+    private int id;
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
